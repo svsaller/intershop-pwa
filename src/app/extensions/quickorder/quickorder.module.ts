a64d3fad3c154f2a408eba7a75efd1c750a243a5
Carola Bratsch <cbratsch@intershop.de>
2021-06-22 06:51:22 +0200

    create a wrapper component to set multiple form fields in a row

--- src/app/extensions/quickorder/quickorder.module.ts
+++ src/app/extensions/quickorder/quickorder.module.ts
@@ -1,12 +1,26 @@
 import { NgModule } from '@angular/core';
+import { ConfigOption, FormlyModule } from '@ngx-formly/core';
 
+import { TextInputFieldComponent } from 'ish-shared/formly/types/text-input-field/text-input-field.component';
+import { ColumnWrapperComponent } from 'ish-shared/formly/wrappers/column-wrapper/column-wrapper.component';
 import { SharedModule } from 'ish-shared/shared.module';
 
 import { HeaderQuickorderComponent } from './shared/header-quickorder/header-quickorder.component';
 
+const quickOrderFormlyConfig: ConfigOption = {
+  types: [
+    {
+      name: 'ish-input-field',
+      component: TextInputFieldComponent,
+      wrappers: ['form-field-column', 'validation'],
+    },
+  ],
+  wrappers: [{ name: 'form-field-column', component: ColumnWrapperComponent }],
+};
+
 @NgModule({
-  imports: [SharedModule],
-  declarations: [HeaderQuickorderComponent],
+  imports: [FormlyModule.forChild(quickOrderFormlyConfig), SharedModule],
+  declarations: [ColumnWrapperComponent, HeaderQuickorderComponent],
   exports: [SharedModule],
 })
 export class QuickorderModule {}
